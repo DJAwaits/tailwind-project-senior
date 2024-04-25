b79b60b3c7b279c52e8982778e825471b16981e7
DJAwaits <padjcreeper@gmail.com>
2024-04-25 14:41:27 -0700

    4/25/2024
    a

--- tailwind-project/src/dashboard/ManageBooks.jsx
+++ tailwind-project/src/dashboard/ManageBooks.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Table } from 'flowbite-react';
 
 const ManageBooks = () => {
+    const [allBooks, setAllBooks] = useState([]);
+    useEffect( () => {
+        fetch("https://localhost:5000/all-books").then(res => res.json()).then(data => setAllBooks(data))
+    }, [])
     return (
-        <div>ManageBooks</div>
+        <div className='px-4 my-12'>
+            <h2 className='mb-8 text-3x1 font-bold'>Manage Your Books</h2>
+
+            {/* table for book data */}
+            <Table className='lg:w-[1180px]'>
+                <Table.Head>
+                    <Table.HeadCell>
+                        No.
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Book Name
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Author
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                       Categrory
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Author
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        <span>
+                            Edit or Manage
+                        </span>
+                    </Table.HeadCell>
+                </Table.Head>
+                {
+                    allBooks.map((book, index) => <Table.Body className="divide-y" key={book._id}>
+
+                    </Table.Body>)
+                }
+                <Table.Body className="divide-y">
+
+                </Table.Body>
+            </Table>
+        </div>
     )
 }
 
--- tailwind-project/src/dashboard/UploadBook.jsx
+++ tailwind-project/src/dashboard/UploadBook.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { clearConfigCache } from 'prettier';
 
 const UploadBook = () => {
     const bookCategories = [
@@ -29,11 +30,44 @@ const UploadBook = () => {
         //  console.log(event.target.value);
         setSelectedBookCategory(event.target.value);
     }
+
+    //  handle book submission
+    const handleBookSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+
+        const bookTitle = form.bookTitle.value;
+        const authorName = form.authorName.value;
+        const imageURL = form.imageURL.value;
+        const category = form.bookCategory.value;
+        const bookDescription = form.bookDescription.value;
+        const bookPDFURL = form.bookPDFURL.value;
+
+        const bookObj = {
+            bookTitle, authorName, imageURL, category, bookDescription, bookPDFURL
+        }
+
+        console.log(bookObj)
+
+        // send data to db
+
+        fetch("http://localhost:5000/upload-book", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(bookObj)
+        }).then(res => res.json()).then(data => {
+            // console.log(data)
+            alert("Book upload successfully!!!")
+            form.reset();
+        })
+    };
     return (
         <div className='px-4 my-12'>
             <h2 className='mb-8 text-3xl font-bold'>Upload A book</h2>
 
-            <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+            <form onSubmit={handleBookSubmit}className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
                 {/* first row */}
                 <div className='flex gap-8'>
                     <div className='lg:w-1/2'>
@@ -97,12 +131,55 @@ const UploadBook = () => {
                             />
                         </div>
 
-                        <Select id='inputState' name='categoryName' className='w-full rounded'>
-
+                        <Select id='inputState' name='categoryName' className='w-full rounded' value={selectedBookCategory}
+                        onChange={handleChangeSelectedValue}>
+                            {
+                                bookCategories.map((option) => <option key={option} value={option}>{option}</option>)
+                            }
                         </Select>
 
                     </div>
                 </div>
+
+                {/* bookDescription */}
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="bookDescription"
+                            value="Book Description"
+                        />
+                    </div>
+                    <Textarea
+                        id="bookDescription"
+                        name="bookDescription"
+                        placeholder="Write your book description..."
+                        required
+                        className='w-full'
+                        rows={4}
+                    />   
+                </div>
+
+                {/* book pdn link */}
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="bookPDFURL"
+                            value="Book PDF URL"
+                        />
+                    </div>
+                    <TextInput
+                        id="bookPDFURL"
+                        name='bookPDFURL'
+                        placeholder="book pdf url"
+                        required
+                        type="text"
+                    />    
+                </div>
+
+                <Button type="submit" className='mt-5'>
+                    Upload Book
+                </Button>
+
             </form>
         </div>
     )
