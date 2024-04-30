55502e43d722bfbd8c86b1abb6ff1d0007597d65
DJAwaits <padjcreeper@gmail.com>
2024-04-30 15:21:22 -0700

    4/30/2024
    a

--- tailwind-project/src/contents/AuthProvider.jsx
+++ tailwind-project/src/contents/AuthProvider.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import app from '../firebase/firebase.config';
+import { getAuth } from "firebase/auth";
+import { useEffect } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
         setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    // const signUpWithGmail = () => {
+    //     return signnInWith
+    // }
+
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser)
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+            
+        }
+    }, [])
+
     const authInfo = {
+        user,
         createUser
     }
     return (
